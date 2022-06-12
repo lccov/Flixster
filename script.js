0919fa2df875830844409fcbe8e90f7c70b92220
lccovington <lacie.covington@gmail.com>
2022-06-12 19:52:05 -0400

    removed unnecessary code

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // GLOBAL CONSTANTS
 const MY_API_KEY = '0dce21e41025b5df140c7122a2a69ba6';
+const scrollUp = document.getElementById("scroll-up");
 
 let form = document.querySelector("form")
 let movieArea = document.querySelector("#movies-grid")
-let showMoreTrendingBtn = document.querySelector("#load-more-movies-btn")
-let showMoreSearchBtn = document.querySelector("#load-more-movies-search-btn")
+let showMoreBtn = document.querySelector("#load-more-movies-btn")
 let userInput = document.querySelector("#search-input")
 let closeBtn = document.querySelector("#close-search-btn")
 let modalContent = document.querySelector("#modal-content")
+
+let trendingFlag = false
+let searchFlag = false 
 
 let pageNum = 1
 
@@ -20,11 +23,13 @@ form.addEventListener("submit", (event) => {
     handleFormSubmission()
 })
 
-showMoreTrendingBtn.addEventListener("click", showMoreTrending)
-showMoreSearchBtn.addEventListener("click", showMoreSearch)
+showMoreBtn.addEventListener("click", showMore)
 closeBtn.addEventListener("click", closeSearch)
 
 async function getTrending() {
+
+    trendingFlag = true
+    searchFlag = false
 
     let url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${MY_API_KEY}&language=en-US&page=${pageNum}`
     let trendingResponse = await fetch(url)
@@ -32,13 +37,6 @@ async function getTrending() {
 
     displayTrending(trendingResponseData)
 
-    if (!showMoreSearchBtn.classList.contains('hidden')) {
-        showMoreSearchBtn.classList.add('hidden')
-    }
-
-    if (showMoreTrendingBtn.classList.contains('hidden')) {
-        showMoreTrendingBtn.classList.remove('hidden')
-    }
 }
 
 function displayTrending(trendingData) {
@@ -56,25 +54,28 @@ function displayTrending(trendingData) {
     });
 
     pageNum += 1
-  }
+}
 
 function closeSearch() {
     pageNum = 1
     movieArea.innerHTML = ``
     loadMore = false
+
     if (!closeBtn.classList.contains('hidden')) {
         closeBtn.classList.add('hidden')
     }
+
     getTrending()
-  }
+}
 
 async function getResults(evt) {
+
+    searchFlag = true
+    trendingFlag = false
 
     if (closeBtn.classList.contains('hidden')) {
         closeBtn.classList.remove('hidden')
     }
-
-    showMoreTrendingBtn.classList.add('hidden')
 
     if (loadMore) {
         searchKey = backupSearchKey
@@ -88,21 +89,13 @@ async function getResults(evt) {
 
     displayResults(searchData)
 
-    if (showMoreSearchBtn.classList.contains('hidden')) {
-        showMoreSearchBtn.classList.remove('hidden')
-    }
-
-    if (!showMoreTrendingBtn.classList.contains('hidden')) {
-        showMoreTrendingBtn.classList.add('hidden')
-    }
-  }
+}
 
 function displayResults(searchData) {
 
     data = searchData
 
     data.results.forEach(movie => {
-        // if else for movie.poster_path
         if (movie.poster_path == null) {
             movieArea.innerHTML += `<div class="movie-card"><img class="movie-poster" src="images/poster-not-found.png" alt="Placeholder image for ${movie.title} movie poster" onclick="showPopUp(${movie.id})" width="312" height="468">
         <div class="movie-info"><p class="movie-title">${movie.title}</p><p class="movie-votes">&#11088;${movie.vote_average}</p></div></div>`
@@ -116,16 +109,16 @@ function displayResults(searchData) {
     userInput.value = ""
 
     pageNum += 1
-  }
-
-function showMoreTrending() {
-    loadMore = true
-    getTrending()
 }
 
-function showMoreSearch() {
-    loadMore = true
-    getResults()
+function showMore() {
+    if (trendingFlag == true && searchFlag == false) {
+        loadMore = true
+        getTrending()
+    } else if (searchFlag == true && trendingFlag == false) {
+        loadMore = true
+        getResults()
+    }
 }
 
 async function showPopUp(movie_id) {
@@ -138,21 +131,19 @@ async function showPopUp(movie_id) {
    let movieVideoResponse = await fetch(videoUrl)
    let movieVideoResponseData = await movieVideoResponse.json()
 
-   //console.log(movieVideoResponseData)
-
    let modal = document.getElementById("moviePopUp");
    let span = document.getElementsByClassName("close")[0];
    modal.style.display = "block";
 
    span.onclick = function() {
     modal.style.display = "none";
-    closePopUp()
+    modalContent.innerHTML = ""
   }
 
   window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
-      closePopUp()
+      modalContent.innerHTML = ""
     }
   }
 
@@ -162,19 +153,12 @@ async function showPopUp(movie_id) {
   <p>Title: ${movieDetailResponseData.title} | Release Date: ${movieDetailResponseData.release_date} | Viewer Rating: ${movieDetailResponseData.vote_average}/10 | Homepage: <a href="${movieDetailResponseData.homepage}">${movieDetailResponseData.homepage}</a></p><p>${movieDetailResponseData.overview}</p>`
 }
 
-function closePopUp() {
-    modalContent.innerHTML = ""
-}
-
 function handleFormSubmission(evt) {
     pageNum = 1
     movieArea.innerHTML = ``
     loadMore = false
-    showMoreTrendingBtn.classList.add('hidden')
-    showMoreSearchBtn.classList.add('hidden')
     getResults(evt);
 }
-const scrollUp = document.getElementById("scroll-up");
 
 scrollUp.addEventListener("click", () => {
     window.scrollTo({
@@ -183,6 +167,7 @@ scrollUp.addEventListener("click", () => {
         behavior: "smooth"
     });
 });
+
 window.onload = function () {
     getTrending();
-  }
+}
